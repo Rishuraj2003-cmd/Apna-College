bd47f0a4a620b9e7429203ecb1fabc5b976dd07a
RISHU RAJ <RR907194@GMAIL.com>
2025-04-05 12:20:50 +0530

    Sorting/InsertionSort.java

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,37 @@
+package Sorting;
+
+public class InsertionSort {
+    public static void insertionSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int key = arr[i];
+            int prev= i-1 ;
+            for(prev = i-1 ; prev>=0;prev--){
+                if (arr[prev]>key) {
+                    arr[prev+1]=arr[prev];
+                }
+                else{
+                    break ;
+                }
+             
+            }
+            arr[prev + 1] = key;
+            
+        }
+
+    }
+
+    public static void PrintSortedArray(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print(arr[i] + "   ");
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 5,1, 6, 0 };
+        insertionSort(arr);
+        PrintSortedArray(arr);
+
+    }
+}
