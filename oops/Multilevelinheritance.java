ad2e2cb719cbc7dde4e71be8293bb84f3dc27857
RISHU RAJ <RR907194@GMAIL.com>
2025-04-23 16:51:56 +0530

    singInheritance.java

---
+++ oops/Multilevelinheritance.java
@@ -0,0 +1,29 @@
+package oops;
+class Gp {
+    void gp() {
+        System.out.println("I am grandparent");
+    }
+}
+
+class par extends Gp {
+    void pa() {
+        System.out.println("I am parent");
+    }
+}
+
+class child extends par {
+    void Child() {
+        System.out.println("I am child");
+    }
+}
+
+public class Multilevelinheritance {
+   
+    public static void main(String[] args) {
+        child obj = new child();
+        System.out.println();
+        obj.Child();
+        obj.pa();
+        obj.gp();
+    }
+}
